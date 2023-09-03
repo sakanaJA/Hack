21b94c7cd311bc75277bfd12939367b7013d8afb
Haku <89026040+sakanaJA@users.noreply.github.com>
2023-09-03 19:55:12 +0900

    hack

--- script.js
+++ script.js
@@ -1,355 +1,60 @@
-let commands = [
-    "ユーザー名確認中... ",
-    "PWを解析中... ",
-    "データベースにアクセス中... ",
-    "システム情報を取得中... ",
-    "セキュリティバイパス中... ",
-    "完了! ",
-    '\n'
-
+const commands = [
+  "ユーザー名確認中...",
+  "PWを解析中...",
+  "データベースにアクセス中...",
+  "システム情報を取得中...",
+  "セキュリティバイパス中...",
+  "完了!",
+  "",
+  "",
+  ""
 ];
-
-function startHacking() {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-
-    if (username && password) {
-        let commandLine = document.querySelector(".command-line");
-        commandLine.textContent = `UserName: ${username}\nPassWord: ${"*".repeat(password.length)}\n`;
-
-        setTimeout(() => {
-            typeCommand(commandLine);
-        }, 1000);
-    } else {
-        alert("ユーザー名とPWを入力してください。");
-    }
-}
 
 let currentCommand = 0;
 let currentChar = 0;
 
-function typeCommand(commandLine) {
-    if (currentChar < commands[currentCommand].length) {
-        commandLine.textContent += commands[currentCommand][currentChar];
-        currentChar++;
-        setTimeout(() => {
-            typeCommand(commandLine);
-        }, Math.random() * 100 + 50);
-    } else {
-        currentCommand++;
-        if (currentCommand < commands.length) {
-            currentChar = 0;
-            commandLine.textContent += '\n';
-            setTimeout(() => {
-                typeCommand(commandLine);
-            }, 1000);
-        }
-    }
-}
-let pythonCodeSnippets = [
-  '\n',
-  "for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()",
-  "for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()",
-  "for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()","for i in range(999):",
-  "    print(i)",
-  "def Haku_world():",
-  "    return 'Haku, World!'",
-  "data = [x**2 for x in range(10)]",
-  "if name == 'main':",
-  "    main()",
-  "class User:",
-  "    def __init__(self, name):",
-  "        self.name = name",
-  "import math",
-  "result = math.sqrt(16)",
-  "with open('file.txt', 'r') as f:",
-  "    content = f.read()"  
-];
-
-function getRandomPythonCode() {
-  return pythonCodeSnippets[Math.floor(Math.random() * pythonCodeSnippets.length)];
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function typeRandomPythonCode(commandLine) {
-  commandLine.textContent += getRandomPythonCode() + '\n'; 
-  if (commandLine.textContent.length < 1000) { 
-      setTimeout(() => {
-          typeRandomPythonCode(commandLine);
-      }, 300); 
+function generateRandomCode() {
+  const structures = [
+      "function ${name}() {",
+      "let ${name} = ${value};",
+      "if (${name} == ${value}) {",
+      "for (let i = 0; i < ${value}; i++) {",
+      "${name}();",
+      "return ${value};",
+      "console.log('${name}');",
+      "}",
+      "}"
+  ];
+
+  const names = ["data", "item", "value", "temp", "result", "info", "user", "config"];
+  const values = ["0", "1", "'sample'", "true", "false", "i", "[]", "{}"];
+
+  let structure = structures[getRandomInt(0, structures.length - 1)];
+  structure = structure.replace("${name}", names[getRandomInt(0, names.length - 1)]);
+  structure = structure.replace("${value}", values[getRandomInt(0, values.length - 1)]);
+
+  return structure;
+}
+
+function typeRandomCode(commandLine) {
+  const randomCode = generateRandomCode() + '\n';
+  commandLine.textContent += randomCode;
+
+  const rect = commandLine.getBoundingClientRect();
+
+  if (rect.height > window.innerHeight) {
+      const lines = commandLine.textContent.split('\n');
+      lines.shift();
+      commandLine.textContent = lines.join('\n');
   }
+
+  setTimeout(() => {
+      typeRandomCode(commandLine);
+  }, getRandomInt(50, 200));
 }
 
 function typeCommand(commandLine) {
@@ -368,7 +73,22 @@ function typeCommand(commandLine) {
               typeCommand(commandLine);
           }, 1000);
       } else {
-          typeRandomPythonCode(commandLine); 
+          typeRandomCode(commandLine);
       }
+  }
+}
+
+function startHacking() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let commandLine = document.querySelector(".command-line");
+
+  if (username && password) {
+      commandLine.textContent = `ユーザー名: ${username}\nPW: ${"*".repeat(password.length)}\n`;
+      setTimeout(() => {
+          typeCommand(commandLine);
+      }, 1000);
+  } else {
+      alert("ユーザー名とPWを入力してください。");
   }
 }
